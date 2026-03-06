fc47f7a66dc87bf01abe62196a70925ae2566284
DevVettel <oenes72658@gmail.com>
2026-03-06 21:02:23 +0300

    node modulleri tarama disi birakildi, Helmet modülü eklendi

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
+// Güvenlik başlıkları
+
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
